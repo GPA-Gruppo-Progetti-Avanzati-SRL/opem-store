0b59441493534bc4f5e551db24ed9ffcfa58ca35
M.A.I. <mario.imperato@gpagroup.it>
2025-10-27 11:46:07 +0100

    aggiornamento migrator

--- scripts/pcweb-data-sets/mongodb/opem-export.sh
+++ scripts/pcweb-data-sets/mongodb/opem-export.sh
@@ -2,20 +2,25 @@
 # export mongoServer=mongodb://localhost:27017/opem
 export mongoServer=mongodb+srv://marioimperato:Y7csjKg4l4c1egiA@maidevfundamentalsclust.x5i5iii.mongodb.net/opem
 
-## apicore_session
-mongoexport --collection=apicore_session    --db=opem  --uri=$mongoServer      --out=exports/apicore_session.json
+## opem_boxes
+mongoexport --collection=opem_boxes          --db=opem  --uri=$mongoServer      --out=exports/opem_boxes.json
+## opem_cards
+mongoexport --collection=opem_cards       --db=opem  --uri=$mongoServer      --out=exports/opem_cards.json
+## opem_file
+mongoexport --collection=opem_file       --db=opem  --uri=$mongoServer      --out=exports/opem_file.json
+## opem_file_rows
+mongoexport --collection=opem_file_rows       --db=opem  --uri=$mongoServer      --out=exports/opem_file_rows.json
+## opem_magazzini
+mongoexport --collection=opem_magazzini      --db=opem  --uri=$mongoServer      --out=exports/opem_magazzini.json
+## opem_organizzazione
+mongoexport --collection=opem_organizzazione --db=opem  --uri=$mongoServer      --out=exports/opem_organizzazione.json
+## opem_persons
+mongoexport --collection=opem_persons       --db=opem  --uri=$mongoServer      --out=exports/opem_persons.json
+## opem_prodotto
+mongoexport --collection=opem_prodotto       --db=opem  --uri=$mongoServer      --out=exports/opem_prodotto.json
 ## opem_system
 mongoexport --collection=opem_system       --db=opem  --uri=$mongoServer      --out=exports/opem_system.json
-## apicore_user
-mongoexport --collection=apicore_user       --db=opem  --uri=$mongoServer      --out=exports/apicore_user.json
-## mag_boxes
-mongoexport --collection=mag_boxes          --db=opem  --uri=$mongoServer      --out=exports/mag_boxes.json
-## mag_magazzini
-mongoexport --collection=mag_magazzini      --db=opem  --uri=$mongoServer      --out=exports/mag_magazzini.json
-## org_organizzazione
-mongoexport --collection=org_organizzazione --db=opem  --uri=$mongoServer      --out=exports/org_organizzazione.json
-## prd_prodotto
-mongoexport --collection=opem_prodotto       --db=opem  --uri=$mongoServer      --out=exports/opem_prodotto.json
-
-
-
+## opem_territorio
+mongoexport --collection=opem_territorio       --db=opem  --uri=$mongoServer      --out=exports/opem_territorio.json
+## opem_user
+mongoexport --collection=opem_user       --db=opem  --uri=$mongoServer      --out=exports/opem_user.json
--- scripts/pcweb-data-sets/mongodb/opem-import.sh
+++ scripts/pcweb-data-sets/mongodb/opem-import.sh
@@ -2,22 +2,29 @@
 # export mongoServer=mongodb://localhost:27017/opem
 # export mongoServer=mongodb+srv://marioimperato:Y7csjKg4l4c1egiA@maidevfundamentalsclust.x5i5iii.mongodb.net/opem
 export mongoServer=$1
-
 mongosh $mongoServer --file drop-collections.js
 
-echo ##### opem_system
-mongoimport --collection=opem_system       --db=opem  --uri=$mongoServer      --file=exports/opem_system.json
-echo ##### apicore_session
-mongoimport --collection=apicore_session    --db=opem  --uri=$mongoServer      --file=exports/apicore_session.json
-echo ##### apicore_user
-mongoimport --collection=apicore_user       --db=opem  --uri=$mongoServer      --file=exports/apicore_user.json
-echo ##### mag_boxes
-mongoimport --collection=mag_boxes          --db=opem  --uri=$mongoServer      --file=exports/mag_boxes.json
-echo ##### mag_magazzini
-mongoimport --collection=mag_magazzini      --db=opem  --uri=$mongoServer      --file=exports/mag_magazzini.json
-echo ##### org_organizzazione
-mongoimport --collection=org_organizzazione --db=opem  --uri=$mongoServer      --file=exports/org_organizzazione.json
-echo ##### opem_prodotto
+## opem_boxes
+mongoimport --collection=opem_boxes          --db=opem  --uri=$mongoServer      --file=exports/opem_boxes.json
+## opem_cards
+mongoimport --collection=opem_cards       --db=opem  --uri=$mongoServer      --file=exports/opem_cards.json
+## opem_file
+mongoimport --collection=opem_file       --db=opem  --uri=$mongoServer      --file=exports/opem_file.json
+## opem_file_rows
+mongoimport --collection=opem_file_rows       --db=opem  --uri=$mongoServer      --file=exports/opem_file_rows.json
+## opem_magazzini
+mongoimport --collection=opem_magazzini      --db=opem  --uri=$mongoServer      --file=exports/opem_magazzini.json
+## opem_organizzazione
+mongoimport --collection=opem_organizzazione --db=opem  --uri=$mongoServer      --file=exports/opem_organizzazione.json
+## opem_persons
+mongoimport --collection=opem_persons       --db=opem  --uri=$mongoServer      --file=exports/opem_persons.json
+## opem_prodotto
 mongoimport --collection=opem_prodotto       --db=opem  --uri=$mongoServer      --file=exports/opem_prodotto.json
+## opem_system
+mongoimport --collection=opem_system       --db=opem  --uri=$mongoServer      --file=exports/opem_system.json
+## opem_territorio
+mongoimport --collection=opem_territorio       --db=opem  --uri=$mongoServer      --file=exports/opem_territorio.json
+## opem_user
+mongoimport --collection=opem_user       --db=opem  --uri=$mongoServer      --file=exports/opem_user.json
 
-mongosh $mongoServer --file create-indexes.js
+mongosh $mongoServer --file fix-mig-02-create-indexes.js
