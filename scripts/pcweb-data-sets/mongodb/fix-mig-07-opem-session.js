2d472e72ff5c80b2b64075bb3c6e37042d37aadf
M.A.I. <mario.imperato@gpagroup.it>
2025-10-31 18:03:11 +0100

    spostamento script di migrazione sotto wrsd-opem

--- scripts/pcweb-data-sets/mongodb/fix-mig-07-opem-session.js
+++
@@ -1,4 +0,0 @@
-print(new Date() + " #################### - fix-mig-07-opem-session.js")
-
-const opemCollectionName = "apicore_session"
-db[opemCollectionName].deleteMany({});
--- scripts/pcweb-data-sets/mongodb/fix-mig-98-cleanup.js
+++
@@ -1,4 +0,0 @@
-
-print(new Date() + " #################### - fix-mig-98-cleanup.js")
-db["opem_addresses"].drop();
-db["opem_cards_apps"].drop();
